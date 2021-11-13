8d68fbc6319245d61a4f18a357512b030491f96d
码帅 <2690438874@qq.com>
2021-11-13 14:01:53 +0800

    完成登录界面Demo

---
+++ src/main/java/com/roadjava/App.java
@@ -0,0 +1,9 @@
+package main.java.com.roadjava;
+
+import main.java.com.roadjava.student.view.LoginView;
+
+public class App {
+    public static void main(String[] args) {
+        new LoginView();
+    }
+}
